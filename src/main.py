d5ed6d114849d7a555b17428da54b9384cb2ebcd
chantulu <elbebe1992k@gmail.com>
2024-06-05 08:52:00 -0700

    handle multiple files

--- src/main.py
+++ src/main.py
@@ -25,7 +25,22 @@ def main():
     # print(tnode)
     copy_static()
     generate_page("../content/index.md","../template.html","../public/index.html")
+    generate_pages_recursive()
     
+
+def generate_pages_recursive(path = "", is_root=True):
+    source = "content"
+    if is_root == True:
+        path=source
+    child_items = []
+    if os.path.isdir(path):
+        child_items = os.listdir(path)
+    for ci in child_items:
+        ci_fp = path + "/" + ci
+        if os.path.isfile(ci_fp) and ci_fp[-3:] == ".md":
+            generate_page(f"../{ci_fp}","../template.html",f"../public{ci_fp[len(source):-2]}html")
+        else:
+            generate_pages_recursive(ci_fp,False)
 
 def extract_title(markdown):
     for line in markdown.splitlines():
@@ -56,7 +71,9 @@ def copy_static(path = "", is_root=True):
         shutil.rmtree(dest)
         os.mkdir(dest)
         path=source
-    child_items = os.listdir(path)
+    child_items = []
+    if os.path.isdir(path):
+        child_items = os.listdir(path)
     for ci in child_items:
         ci_fp = path + "/" + ci
         if os.path.isfile(ci_fp):
